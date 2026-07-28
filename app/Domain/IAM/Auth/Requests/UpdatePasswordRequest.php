a7520d3814b821411ace3d77de5c2eaba00fd48f
ahmdkenz <gigibengal14@gmail.com>
2026-07-28 17:29:15 +0700

    Update Mandatory Min Char Password

--- app/Domain/IAM/Auth/Requests/UpdatePasswordRequest.php
+++ app/Domain/IAM/Auth/Requests/UpdatePasswordRequest.php
@@ -15,7 +15,7 @@ class UpdatePasswordRequest extends FormRequest
     {
         return [
             'current_password'          => ['required', 'string'],
-            'new_password'              => ['required', 'string', 'min:8', 'confirmed'],
+            'new_password'              => ['required', 'string', 'confirmed'],
             'new_password_confirmation' => ['required', 'string'],
         ];
     }
@@ -25,7 +25,6 @@ class UpdatePasswordRequest extends FormRequest
         return [
             'current_password.required'          => 'Password saat ini wajib diisi.',
             'new_password.required'              => 'Password baru wajib diisi.',
-            'new_password.min'                   => 'Password baru minimal 8 karakter.',
             'new_password.confirmed'             => 'Konfirmasi password tidak cocok.',
             'new_password_confirmation.required' => 'Konfirmasi password wajib diisi.',
         ];
