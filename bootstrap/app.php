e28d66f7a5d4872083d559f595a246397edf0bcc
ahmdkenz <gigibengal14@gmail.com>
2026-08-13 11:45:21 +0700

    Fix  Gap Security

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -74,6 +74,8 @@ return Application::configure(basePath: dirname(__DIR__))
             ->withoutOverlapping();
     })
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
+
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \App\Http\Middleware\InjectCookieToken::class,
